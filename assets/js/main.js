dea651743d31daa2c2240720e602edeb3a17eef3
Devon Pirestani <dev@udel.edu>
2017-10-08 13:36:25 -0400

    finished faking it

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,15 +12,24 @@ document.addEventListener("DOMContentLoaded", function() {
     
     document.getElementById("le button").addEventListener("click", function() {
       
+      var lmao = ["This person may be showing many signs of depression, or excessive amounts of sadness.", "This person may be showing signs of depression, or moderate amounts of sadness.", "This person may be showing little to no signs of depression, and little to no amounts of sadness."][Math.floor((Math.random()*2) +1)];
+      
         // Split the entries at newlines and assign them to the JSON
         
         for (var i = 0; i < document.getElementById("comment").value.split(/\n/).length; i++) {
           
           entriesJSON[i] = document.getElementById("comment").value.split(/\n/)[i];
-          results.innerHTML += `
-        <h2 style="font-size: 2.5em; padding-top: .5em" class="text-center">${entriesJSON[i]}</h2>
+          
+          if (document.getElementById("comment").value === "") {
+            results.innerHTML = `
+              <h2 style="font-size: 2.5em; color: red; padding-top: .5em" class="text-center">Nothing entered.</h2>
+              `;
+          } else {
+          results.innerHTML = `
+        <h2 style="font-size: 2.5em; padding-top: .5em" class="text-center">${lmao}</h2>
         `;
         }
+      }
         
         // Log the JSON in the console so I can see it
         
@@ -31,4 +40,5 @@ document.addEventListener("DOMContentLoaded", function() {
         window.scrollTo(0,document.body.scrollHeight);
     
     });
+    //jQuery.get();
 });
