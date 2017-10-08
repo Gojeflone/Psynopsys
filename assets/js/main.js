efed9c0fa1e8654287eb2e15f1534e91c7afd6c6
Devon Pirestani <dev@udel.edu>
2017-10-08 12:05:10 -0400

    JS returns JSON instead of Array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
+  
     // Get the ID of the result element
+    
     var results = document.getElementById("result");
+    
     // Make an empty array for the entries
-    var entriesJSON = [];
+    
+    var entriesJSON = {};
+    
     // When the analyze button is clicked...
+    
     document.getElementById("le button").addEventListener("click", function() {
-        // Split the entries newlines and assign them to entriesArray
-        entriesJSON = document.getElementById("comment").value.split(/\n/);
-        // Log the array in the console so I can see it
-        console.log(entriesArray);
-        // Put the results in the HTML
-        results.innerHTML = `
-        <h2 style="font-size: 3em; padding-top: .5em" class="text-center"><strong>Result:</strong></h2>
-        <h2 style="text-decoration: underline; font-size: 2.5em; padding-top: .5em" class="text-center">Enter Results</h2>
-        `
+      
+        // Split the entries at newlines and assign them to the JSON
+        
+        for (var i = 0; i < document.getElementById("comment").value.split(/\n/).length; i++) {
+          
+          entriesJSON[i] = document.getElementById("comment").value.split(/\n/)[i];
+          results.innerHTML += `
+        <h2 style="font-size: 2.5em; padding-top: .5em" class="text-center">${entriesJSON[i]}</h2>
+        `;
+        }
+        
+        // Log the JSON in the console so I can see it
+        
+        console.log(entriesJSON);
+        
         // Scroll to the bottom of the page to see the results
+        
         window.scrollTo(0,document.body.scrollHeight);
     
     });
