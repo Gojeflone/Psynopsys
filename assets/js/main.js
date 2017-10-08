8cafa780d488485d97ff51e08c35a3b120ad7c56
Devon Pirestani <dev@udel.edu>
2017-10-08 06:50:48 -0400

    commented code for readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Get the ID of the result element
     var results = document.getElementById("result");
+    // Make an empty array for the entries
     var entriesArray = [];
+    // When the analyze button is clicked...
     document.getElementById("le button").addEventListener("click", function() {
+        // Split the entries newlines and assign them to entriesArray
         entriesArray = document.getElementById("comment").value.split(/\n/);
+        // Log the array in the console so I can see it
         console.log(entriesArray);
+        // Put the results in the HTML
         results.innerHTML = `
         <h2 style="font-size: 3em; padding-top: .5em" class="text-center"><strong>Result:</strong></h2>
         <h2 style="text-decoration: underline; font-size: 2.5em; padding-top: .5em" class="text-center">You\'re fuckin\' gay</h2>
         `
+        // Scroll to the bottom of the page to see the results
         window.scrollTo(0,document.body.scrollHeight);
-        //
-        var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
-        
-        var tone_analyzer = new ToneAnalyzerV3({
-          username: '{765733e9-c353-46ac-a622-35a614caf5fe}',
-          password: '{mYsZDMBl1wBY}',
-          version_date: '{2017-09-28}',
-          headers: {
-            'X-Watson-Learning-Opt-Out': 'true'
-          }
-        });
-        
-        var params = {
-          // Get the text from the JSON file.
-          text: require('test.json').text,
-          tones: 'emotion'
-        };
-        
-        tone_analyzer.tone(params, function(error, response) {
-          if (error)
-            console.log('error:', error);
-          else
-            console.log(JSON.stringify(response, null, 2));
-          }
-        );
-        //
+    
     });
 });
